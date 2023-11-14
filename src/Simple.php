10fc8978dc8b2e14f136609742743d8f71aee681
Marcos Alexandre Matos Marques <marcos.marques.m@bairesdev.com>
2023-11-14 11:31:42 -0400

    minor updates

--- src/Simple.php
+++ src/Simple.php
@@ -19,4 +19,24 @@ class Simple
 
     	return $this->number / $divisor;
 	}
+
+	public function multiply(float|int $number){
+		return $this->number * $number;
+	}
+
+	public function add(float|int $number){
+		return $this->number + $number;
+	}
+
+	public function subtract(float|int $number){
+		return $this->number - $number;
+	}
+
+	public function power($exponent){
+		return $this->number ** $exponent;
+	}
+
+	public function sqrt(){
+		return sqrt($this->number);
+	}
 }
--- tests/SimpleTest.php
+++ tests/SimpleTest.php
@@ -13,4 +13,72 @@ class SimpleTest extends TestCase
 
 		$this->assertEquals(5, $result);
 	}
+
+	public function testDivideBigNumber()
+	{
+		$simple = new Simple(200_000_000_000);
+		$result = $simple->divide(2);
+
+		$this->assertEquals(100_000_000_000, $result);
+	}
+
+	public function testDivideNegative()
+	{
+		$simple = new Simple(20);
+		$result = $simple->divide(-2);
+
+		$this->assertEquals(-10, $result);
+	}
+
+	public function testDivideNegativeBigNumber()
+	{
+		$simple = new Simple(200_000_000_000);
+		$result = $simple->divide(-2);
+
+		$this->assertEquals(-100_000_000_000, $result);
+	}
+
+	public function testDivideNegativeByNegative()
+	{
+		$simple = new Simple(-20);
+		$result = $simple->divide(-2);
+
+		$this->assertEquals(10, $result);
+	}
+
+	public function testDivideByZero(){
+		$simple = new Simple(20);
+		$this->expectException(\InvalidArgumentException::class);
+		$simple->divide(0);
+	}
+
+	public function testMultiply()
+	{
+		$simple = new Simple(10);
+		$result = $simple->multiply(2);
+
+		$this->assertEquals(20, $result);
+	}
+
+	public function testSubtract(){
+		$simple = new Simple(12);
+		$result = $simple->subtract(1);
+
+		$this->assertEquals(1, $result);
+	}
+
+	public function testePower(){
+		$simple = new Simple(3);
+		$result = $simple->power(2);
+
+		$this->assertEquals(9, $result);
+	}
+
+	public function testSqrt(){
+		$simple = new Simple(9);
+		$result = $simple->sqrt();
+
+		$this->assertEquals(3, $result);
+	}
+
 }
